61f760030d2f6e66235e2505f63ac18b226593ff
thao3005 <55047525+thao3005@users.noreply.github.com>
2024-03-20 13:33:53 -0600

    camera + basic features

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     const video = document.getElementById('video');
+
+//     if (navigator.mediaDevices.getUserMedia) {
+//         navigator.mediaDevices.getUserMedia({ video: true })
+//             .then(function(stream) {
+//                 video.srcObject = stream;
+//             })
+//             .catch(function(error) {
+//                 console.log("Something went wrong when accessing the camera!");
+//             });
+//     }
+// });
